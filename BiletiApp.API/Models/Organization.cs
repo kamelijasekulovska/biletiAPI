c3d70bc63195947c13891d0a019c9a3ac1175c11
Eleni Mickovska <eleni.mickovska@uptime.mk>
2019-09-02 11:25:43 +0200

    Organization Model

--- BiletiApp.API/Models/Organization.cs
+++ BiletiApp.API/Models/Organization.cs
@@ -7,5 +7,13 @@ namespace BiletiApp.API.Models
 {
     public class Organization
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Contact[] Contacts { get; set; } 
+        public string Description { get; set; }
+        public string Logo { get; set; }
+        public string Cover { get; set; }
+        public Image[] Gallery { get; set; }
+        public Boolean Enabled { get; set; }
     }
 }
