1c61f5bc8ad31c9b4135cd82ba6aeb7e40f7a561
Kamelija Sekulovska <kamelija.sekulovska@uptime.mk>
2019-09-02 11:36:47 +0200

    User Model (no role Id), Venue model

--- BiletiApp.API/Models/User.cs
+++ BiletiApp.API/Models/User.cs
@@ -7,5 +7,11 @@ namespace BiletiApp.API.Models
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public Contact Contact { get; set; }
+        public int ContactId { get; set; }
+        public string Password { get; set; }
+        public Organization Organization { get; set; }
+        public int OrganizationId { get; set; }
     }
 }
--- BiletiApp.API/Models/Venue.cs
+++ BiletiApp.API/Models/Venue.cs
@@ -7,5 +7,11 @@ namespace BiletiApp.API.Models
 {
     public class Venue
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Contact[] Contacts { get; set; }
+        public int Capacity { get; set; }
+        public Sector[] Sectors { get; set; }
     }
 }
