91a3cc52a16b77b6d2df7620d055862db214fc5b
Eleni Mickovska <eleni.mickovska@uptime.mk>
2019-09-02 11:40:48 +0200

    Transaction Model

--- BiletiApp.API/Models/Transaction.cs
+++ BiletiApp.API/Models/Transaction.cs
@@ -7,5 +7,10 @@ namespace BiletiApp.API.Models
 {
     public class Transaction
     {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Ticket Ticket { get; set; }
+        public int TicketId { get; set; }
     }
 }
