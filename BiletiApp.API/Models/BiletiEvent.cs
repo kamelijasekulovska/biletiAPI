064444c345f7b3b607c7a45c25d4691b757cb15d
Kamelija Sekulovska <kamelija.sekulovska@uptime.mk>
2019-09-02 11:28:02 +0200

    BiletiEvent model, Seat model

--- BiletiApp.API/Models/BiletiEvent.cs
+++ BiletiApp.API/Models/BiletiEvent.cs
@@ -9,5 +9,16 @@ namespace BiletiApp.API.Models
     {
         public string Name { get; set; }
         public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Tag[] Tags { get; set; }
+        public Image[] Images { get; set; }
+        public string Cover { get; set; }
+        public int Max_tickets_per_account { get; set; }
+        public Organization Organization { get; set; }
+        public int OrganizationId { get; set; }
+        public Venue Venue { get; set; }
+        public int VenueId { get; set; }
+
     }
 }
