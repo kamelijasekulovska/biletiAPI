6ea3a1a50fda5688aebe51d015f94ad5d31a8b1a
Eleni Mickovska <eleni.mickovska@uptime.mk>
2019-09-02 11:34:14 +0200

    Sector Model

--- BiletiApp.API/Models/Sector.cs
+++ BiletiApp.API/Models/Sector.cs
@@ -7,5 +7,12 @@ namespace BiletiApp.API.Models
 {
     public class Sector
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public Boolean Seated { get; set; }
+        public int Capacity { get; set; }
+        public int Rows { get; set; }
+        public Seat[] Seats { get; set; }
     }
 }
